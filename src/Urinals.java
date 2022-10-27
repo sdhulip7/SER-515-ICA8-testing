06660e26030a9c6ad3e8f1e6c32d11fcdd760354
sdhulip7 <sdhulip7@asu.edu>
2022-10-27 13:58:02 -0700

    Added skeleton for all the functions to be implemented

--- src/Urinals.java
+++ src/Urinals.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 
 //Author - Sri Venkata Vivek Dhulipala
 public class Urinals {
-    public static void main(String[] args) {
 
+    Boolean isValidString(String str) { // checks if the String is valid and returns -1.
+        System.out.println("Not yet implemented");
+        return true;
+    }
+
+    public static void main(String[] args) { // prints the available urinals count.
+        System.out.println("Not yet implemented");
     }
 }
