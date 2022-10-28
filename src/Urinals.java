0cab5526f4590844c6529b46dad878b67fb41f13
sdhulip7 <sdhulip7@asu.edu>
2022-10-27 17:31:48 -0700

    Wrote function skeleton for countUrinals

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,18 +1,18 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 //Author - Sri Venkata Vivek Dhulipala
 public class Urinals {
 
-    static Boolean isValidString(String str) { // checks if the String is valid and returns -1.
+    static boolean isValidString(String str) { // checks if the String is valid and returns -1.
         int n = str.length();
-        Boolean flag=true;
+        boolean flag=true;
         for(int i=0;i<n;i++) {
             if(str.charAt(i) != '0' || str.charAt(i) !='1') {
                 flag=false;
             }
             if(i!=n-1) {
-                if (str.charAt(i) == '1' && str.charAt(i+1) != '1') {
+                if (str.charAt(i) == '1' && str.charAt(i+1) == '1') {
                     flag=false;
                 }
             }
@@ -20,9 +20,9 @@ public class Urinals {
         return flag;
     }
 
-
-
-
+    int urinalsCount(String str) { // checks the number of available urinals
+        return 0;
+    }
 
     public static void main(String[] args) { // prints the available urinals count.
         System.out.println("Not yet implemented");
