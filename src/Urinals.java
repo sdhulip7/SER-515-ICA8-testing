be9afc0d99acc5637078d0137577a8719e8c63c9
sdhulip7 <sdhulip7@asu.edu>
2022-10-27 17:05:45 -0700

    Corrected isValidString method so that testIsValidString passes

--- src/Urinals.java
+++ src/Urinals.java
@@ -5,9 +5,24 @@ import java.util.Scanner;
 public class Urinals {
 
     static Boolean isValidString(String str) { // checks if the String is valid and returns -1.
-        System.out.println("Not yet implemented");
-        return true;
+        int n = str.length();
+        Boolean flag=true;
+        for(int i=0;i<n;i++) {
+            if(str.charAt(i) != '0' || str.charAt(i) !='1') {
+                flag=false;
+            }
+            if(i!=n-1) {
+                if (str.charAt(i) == '1' && str.charAt(i+1) != '1') {
+                    flag=false;
+                }
+            }
+        }
+        return flag;
     }
+
+
+
+
 
     public static void main(String[] args) { // prints the available urinals count.
         System.out.println("Not yet implemented");
